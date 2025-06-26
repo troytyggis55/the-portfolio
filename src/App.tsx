6106bc1b24718f0684ca0b26684dbd62fc11dd6b
Trygve Jørgensen <trygveabjo@gmail.com>
2025-06-26 23:25:12 +0200

    Added generalized styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 import Test from './test.mdx'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-        <Test/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            
+            <header className="h-32 sm:h-header fixed top-0 left-0 w-full shadow-lg z-50 flex justify-center bg-linear-to-b from-accent/10 to-transparent border-b border-accent/40 backdrop-blur-2xl">
+                <div className="flex flex-col gap-1 w-app p-8 justify-center">
+                    <span className="text-4xl sm:text-5xl font-semibold">Trygve Jørgensen</span>
+                    <span className="text-xl sm:text-2xl">Utvikler og Student</span>
+                </div>
+            </header>
+            <div className="h-32 sm:h-header"></div>
+            
+            <Test/>
+            <Test/>
+            <Test/>
+            <Test/>
+            <div>
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+        </>
+    )
 }
 
 export default App
